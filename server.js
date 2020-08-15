0e3475225860c0b5e726460574b3c14135411e0a
Sumit Samanta <sumit.samanta@jungleworks.com>
2020-08-15 20:45:15 +0530

    change in the server file

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const path = require("path");
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.use('/api', router);
-server.listen(port, () => {
-     console.log(`JSON Server is listening on port ${port}`);
-});
+
+server.listen(port);
