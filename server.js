09befb87eb83274633d7d3c646879a2d6490e94c
Sumit Samanta <sumit.samanta@jungleworks.com>
2020-08-16 00:25:10 +0530

    add new api and express server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const jsonServer = require('json-server');
+const jsonServer = require('./json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`Test Custom Data API listening on port ${port}!`)
+});
