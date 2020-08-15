aa14968ccc41da94f6e136e184e8d62283f4c5cc
Sumit Samanta <sumit.samanta@jungleworks.com>
2020-08-15 20:31:47 +0530

    change listen port

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const server = jsonServer.create();
 const path = require("path");
 const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
-const port = 3004;
+const port = 8081;
 
 server.use(middlewares);
 server.use(router);
 server.use('/api', router);
-server.listen(port, () => {
+server.listen(process.env.PORT || port, () => {
      console.log(`JSON Server is listening on port ${port}`);
 });
